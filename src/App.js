04b43239eaa1fe99f4536d70190587f43d695303
Nick M. Jaremek <nick13jaremek@gmail.com>
2018-05-17 23:27:37 +0200

    Refactored Search component to be a 'functionally stateless component'. That is, it only receives props, and returns JSX content.

--- src/App.js
+++ src/App.js
@@ -70,21 +70,15 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}
-        <input 
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onChange, children }) =>
+    <form>
+      {children}
+      <input 
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </form>
 
 class Table extends Component {
   render() {
@@ -92,19 +86,19 @@ class Table extends Component {
     return (
       <div>
         {list.filter(isSearched(pattern)).map(item => 
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <Button onClick={() => onDismiss(item.objectID)}>
-                  Dismiss
-                </Button>
-              </span>
-            </div>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
+            </span>
+          </div>
         )}
       </div>
     );
