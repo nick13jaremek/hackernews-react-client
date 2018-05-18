31a1313be213cb08dc5fe960eccbd2683cea9c7c
Nick M. Jaremek <nick13jaremek@gmail.com>
2018-05-18 17:59:37 +0200

    Added styling for list items.

--- src/App.js
+++ src/App.js
@@ -86,17 +86,29 @@ const Search = ({ value, onChange, children }) => {
 }
 
 const Table = ({ list, pattern, onDismiss }) => {
+  const largeColumnStyle = {
+    width: '40%',
+  };
+
+  const midColumnStyle = {
+    width: '30%',
+  };
+
+  const smallColumnStyle = {
+    width: '10%',
+  };
+
   return(
     <div className="table">
       {list.filter(isSearched(pattern)).map(item => 
         <div key={item.objectID} className="table-row">
-          <span>
+          <span style={largeColumnStyle}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
+          <span style={midColumnStyle}>{item.author}</span>
+          <span style={smallColumnStyle}>{item.num_comments}</span>
+          <span style={smallColumnStyle}>{item.points}</span>
+          <span style={smallColumnStyle}>
             <Button
               onClick={() => onDismiss(item.objectID)}
               className="button-inline"
