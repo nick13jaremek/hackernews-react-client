c71871df9f74d7e457f91a4c3f4e5292b49559f9
Nick M. Jaremek <nick13jaremek@gmail.com>
2018-05-20 13:22:33 +0200

    Added Enzyme test for Search component.

--- src/components/Search/test.js
+++ src/components/Search/test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import Search from './index.js';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Search', () => {
   const props = {
@@ -23,6 +27,14 @@ describe('Search', () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should contain the proper children', () => {
+    const element = shallow(
+      <Search { ...props } />
+    );
+
+    expect(element.contains('Search')).toEqual(true);
   });
 });
 
