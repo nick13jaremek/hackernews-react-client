9b552116e7be15d8753c2ed80f51d2ebd8a253c0
Nick M. Jaremek <nick13jaremek@gmail.com>
2018-05-20 13:14:09 +0200

    Added onDismiss prop to test logic of Table component.

--- src/components/Table/test.js
+++ src/components/Table/test.js
@@ -13,6 +13,7 @@ describe('Table', () => {
       { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
       { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
     ],
+    onDismiss: () => true,
   };
 
   it('renders without crashing', () => {
